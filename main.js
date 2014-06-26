adef6aec9dfd62040d38409e7bd145f641aef76b
ryandagg <ryan.dagg@gmail.com>
2014-06-26 17:42:08 -0600

    HTML & CSS done, can hide & show update section

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $(document).on('ready', function() {
-  
+  $(".submitBtn").on('click', function(){
+  	// console.log("click")
+  	$(".profileUpdate").show();
+  	$(".submitBtn").text("Submit & Hide")
+
+  	// submit information & udate static profile
+  	$(".submitBtn").on("click", function(){
+  		// console.log("click")
+  		$(".profileUpdate").hide();
+  		$(".submitBtn").text("Update");
+  	})
+  })
 });
